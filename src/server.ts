ca6ad1f4c3a081de21e4a8cbdb0600e1c3503028
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-03-27 22:57:39 +0900

    Set up CI with Azure Pipelines (#11)
    * Set up CI with Azure Pipelines
    
    * fix: Fix CI Settings
    
    * fix: Add "dom" lib to compilerOptions.lib
    
    * fix: Add jest-junit
    
    * refactor: Remove CircleCI Settings

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import { app } from "./app";
 app.use(errorHandler());
 
 const server = app.listen(app.get("port"), () => {
+  /* eslint-disable */
   console.log(
     "  App is running at http://localhost:%d in %s mode",
     app.get("port"),
     app.get("env")
   );
   console.log("  Press CTRL-C to stop\n");
+  /* eslint-enable */
 });
 
 export default server;
